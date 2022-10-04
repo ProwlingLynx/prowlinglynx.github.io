5463188bc97f5bbe3413923238d0725d2339a3e8
ProwlingLynx <tamezjavier@gmail.com>
2022-10-04 01:32:18 -0500

    Adding init Bio component. Verified successful render.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import {NavBar} from "./components/NavBar";
+import {Bio} from "./components/Bio";
 import './App.css';
+
+
+/**
+ * Renders the app views.
+ * TODO: migrate to React Router
+ * @returns {JSX.Element}
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Bio />
+    </>
   );
 }
 
