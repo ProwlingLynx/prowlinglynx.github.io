4c08456e68409f7e9636db6d54a1a29aa2e86aa9
ProwlingLynx <tamezjavier@gmail.com>
2022-10-04 02:00:18 -0500

    Adding bio section.

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,3 +1,13 @@
 import styles from "./Bio.module.css";
 
-export const Bio = () => {};
+export const Bio = () => {
+  return (
+    <div className={styles.bio}>
+      <h2>Bio</h2>
+      <article>
+        <p>I am an easy going fellow who enjoys gaming and having a fun time. At work I strive to make sure everyone comes out a winner. Even when the going gets tough.</p>
+        <p>I enjoy a good fireside chat and meeting new people, so reach out and say hello. We might even become good friends.</p>
+      </article>
+    </div>
+  );
+};
