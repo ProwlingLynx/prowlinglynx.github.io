2c6cea7dcbab9e7b2d2a7b45d86047b28c9ce511
Javier Tamez <tamezjavier@gmail.com>
2023-03-21 23:37:52 -0500

    Adding menu to nav bar.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,29 @@
 import styles from './NavBar.module.css';
+import { useRef } from 'react';
+
 export const NavBar = () => {
+  const menuRef = useRef(null);
+  const menuHandler = (e) => {
+    e.preventDefault();
+    const target = menuRef.current;
+    if (target.style.display === 'block') {
+      target.style.display = 'none';
+    } else {
+      target.style.display = 'block';
+    }
+  };
   return (
-    <nav className={styles.NavBar}>
-      <h2>Home</h2>
-      <h2>About</h2>
-      <h2>Contact Me</h2>
-      <h2>Projects</h2>
-    </nav>
+    <header className='topnav'>
+      <a href='#home' className='active'>
+        JT's Lair
+      </a>
+      <nav id='myNavs' ref={menuRef}>
+        <a href='#contact'>Contact Me</a>
+        <a href='#projects'>Projects</a>
+      </nav>
+      <a className={`icon`} onClick={menuHandler}>
+        =
+      </a>
+    </header>
   );
 };
