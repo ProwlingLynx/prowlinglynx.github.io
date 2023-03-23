e766d6f615a889278654c1d6f4d3ec1c1d17c03f
Javier Tamez <tamezjavier@gmail.com>
2023-03-23 18:01:40 -0500

    Updating styling for introduction component. Small view and large view.

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -2,18 +2,22 @@ import styles from './Greeting.module.css';
 
 export const Greeting = () => {
   return (
-    <div className={`${styles.greeting} flex-column`}>
-      <article>
-        <h1>Hi! Welcome to my portfolio.</h1>
-        <p>My name is Javier Tamez.</p>
-        <p>
-          I am a full stack web developer that specializes in creating Typescript and Javascript
-          based solutions. My skill set is always expanding so feel free to reach out even if your
-          needs extend beyond the Javascript ecosystem.
-        </p>
-        <button>Get in touch</button>
-      </article>
-      <img src={'https://avatars.githubusercontent.com/u/91586098?v=4'} alt='Javier Tamez' />
-    </div>
+    <article className={`${styles.greeting}`}>
+      <span className={`flex-row ${styles.imageContainer}`}>
+        <h1 className={`${styles.greetHeader}`}>Hi and Welcome!</h1>
+        <img
+          className={`${styles.greetPicture}`}
+          src={'https://avatars.githubusercontent.com/u/91586098?v=4'}
+          alt='Javier Tamez'
+        />
+      </span>
+      <p>My name is Javier Tamez.</p>
+      <p className={`indent-p`}>
+        I am a full stack web developer that specializes in creating Typescript and Javascript based
+        solutions. My skill set is always expanding so feel free to reach out even if your needs
+        extend beyond the Javascript ecosystem.
+      </p>
+      <button className={`${styles.greetingButton}`}>Get in touch</button>
+    </article>
   );
 };
